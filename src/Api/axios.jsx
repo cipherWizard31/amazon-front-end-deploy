83265ff5a1ecd0c52dcb7f7a83d681e11a3530e2
Natan Yidnekachew <164697270+cipherWizard31@users.noreply.github.com>
2024-10-20 23:43:23 +0300

    Update on base url

--- src/Api/axios.jsx
+++ src/Api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL:"http://localhost:7777"
+    baseURL:"https://amazon-api-deploy-0i1o.onrender.com"
 })
 
 export {axiosInstance}
